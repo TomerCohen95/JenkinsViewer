3993b77612445eaa328d5e17c4134308db3d337c
Tomer Cohen QA <tomerco@deepinstinct.com>
2020-10-08 10:15:35 +0300

    new

--- config.py
+++ config.py
@@ -7,8 +7,7 @@ class Config(object):
     DEBUG = False
     TESTING = False
     CSRF_ENABLED = True
-    SECRET_KEY = 'this-really-needs-to-be-changed'
-    SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
+    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL", "sqlite://")
 
 
 class ProductionConfig(Config):
--- manage.py
+++ manage.py
@@ -1,16 +1,17 @@
-import os
-from flask_script import Manager
-from flask_migrate import Migrate, MigrateCommand
-from alembic import op
-import sqlalchemy as sa
-from app import app, db
+from flask.cli import FlaskGroup
 
-app.config.from_object(os.environ['APP_SETTINGS'])
+import app
+from app import db
 
-migrate = Migrate(app, db, compare_type=True)
-manager = Manager(app)
+cli = FlaskGroup(app)
 
-manager.add_command('db', MigrateCommand)
 
-if __name__ == '__main__':
-    manager.run()
+@cli.command("create_db")
+def create_db():
+    db.drop_all()
+    db.create_all()
+    db.session.commit()
+
+
+if __name__ == "__main__":
+    cli()
